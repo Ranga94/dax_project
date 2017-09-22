8be657ebac7d9ca4b8a52a89a634d9377a2c6695
ulince <4fe84.2Y>
2017-09-22 10:49:42 +0100

    Adding path

--- twitter_analytics/twitter_mining.py
+++ twitter_analytics/twitter_mining.py
@@ -1,6 +1,6 @@
 import sys
-from pathlib import Path
-sys.path.insert(0, str(Path('..', 'utils')))
+#from pathlib import Path
+#sys.path.insert(0, str(Path('..', 'utils')))
 from DB import DB
 import tweepy
 from nltk.tokenize import TweetTokenizer
