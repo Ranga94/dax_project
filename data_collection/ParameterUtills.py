8cf92f1e00e30c77fcafe1348e10fac99ca4e487
info@igenieconsulting.com <info@igenieconsulting.com>
2017-10-26 16:09:30 +0000

    Added parameter utils class

--- data_collection/ParameterUtills.py
+++ data_collection/ParameterUtills.py
@@ -4,44 +4,33 @@ class ParameterUtils:
     def __init__(self):
         pass
 
-    def get_parameters(sql_connection_string=None, sql_table=None ,mongo_connection_string=None):
+    def get_parameters(self,sql_connection_string=None, sql_table_name=None,
+        mongo_connection_string=None, sql_column_list=None):
         if sql_connection_string:
             engine = create_engine(sql_connection_string)
             metadata = MetaData(engine)
 
-            param_twitter_collection = Table('PARAM_TWITTER_COLLECTION', metadata, autoload=True)
+            source_table = Table(sql_table_name, metadata, autoload=True)
 
+            projection_columns = [c for c in source_table.columns if str(c) in sql_column_list]
 
+            statement = select(projection_columns)
+            result = statement.execute()
+            row = result.fetchone()
+            result.close()
+            return row
+        
 
-
-
-        statement = select([param_twitter_collection.c.LANGUAGE,
-                            param_twitter_collection.c.TWEETS_PER_QUERY,
-                            param_twitter_collection.c.MAX_TWEETS,
-                            param_twitter_collection.c.CONNECTION_STRING,
-                            param_twitter_collection.c.DATABASE_NAME,
-                            param_twitter_collection.c.COLLECTION_NAME,
-                            param_twitter_collection.c.LOGGING_FLAG])
-        result = statement.execute()
-        row = result.fetchone()
-        result.close()
-        return row
+        
 
 if __name__ == "__main__":
     from sqlalchemy.sql import column
     p = ParameterUtils()
-    engine = create_engine("mysql+pymysql://igenie_readwrite:igenie@35.197.246.202/dax_project")
-    metadata = MetaData(engine)
+    x = ["PARAM_TWITTER_COLLECTION.LANGUAGE","PARAM_TWITTER_COLLECTION.DATABASE_NAME"]
 
-    param_twitter_collection = Table('PARAM_TWITTER_COLLECTION', metadata, autoload=True)
-    statement = select([param_twitter_collection.c.LANGUAGE,
-                        param_twitter_collection.c.TWEETS_PER_QUERY,
-                        param_twitter_collection.c.MAX_TWEETS,
-                        param_twitter_collection.c.CONNECTION_STRING,
-                        param_twitter_collection.c.DATABASE_NAME,
-                        param_twitter_collection.c.COLLECTION_NAME,
-                        param_twitter_collection.c.LOGGING_FLAG])
-    print(type(statement))
-    result = statement.execute()
-    row = result.fetchone()
-    result.close()
+    p.get_parameters(sql_connection_string="mysql+pymysql://igenie_readwrite:igenie@35.197.246.202/dax_project",
+        sql_table_name="PARAM_TWITTER_COLLECTION", 
+        sql_column_list=x)
+    
+
+    
