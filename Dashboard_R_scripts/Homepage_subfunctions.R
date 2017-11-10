eff4e9246479d9bcc563757c67aacc1b2893df87
keeffa <hu.kefei@yahoo.co.uk>
2017-11-10 10:46:08 +0000

    Restructured R scripts for dashboard

---
+++ Dashboard_R_scripts/Homepage_subfunctions.R
@@ -0,0 +1,105 @@
+##Stores all the functions used for the Homepage of Analytics Dashboard. 
+
+#This function counts the number of positive, negative and neutral tweets for one specific stock. 
+tweet_count<-function(constituent_count){
+  
+  pos_line = constituent_count[constituent_count$line=='Positive',c("count")]
+  neg_line = constituent_count[constituent_count$line=='Negative',c("count")]
+  neu_line = constituent_count[constituent_count$line=='Neutral',c("count")]
+  constituent = constituent_count$constituent[1]
+  title_str = paste('Number of Tweets for ',constituent,sep='')
+  
+  if (constituent =='EON'){
+  neg_line <- append(neg_line,0,after=min(neg_line))}
+  
+  date_range <- constituent_count[constituent_count$line=='Neutral',c("date")]
+  date_range <- as.Date(date_range,"%Y-%m-%d")
+  date_range<-gsub("-", "/", date_range)
+  date_range <- as.Date(date_range,"%Y/%m/%d")
+  
+  constituent_count_posi<-constituent_count[constituent_count$line=='Positive',c("count",'date')]
+  ggplot(data=constituent_count_posi)+
+    geom_line(aes(x=date_range,y=pos_line,group=1,color="Positive")) +geom_point(aes(x=date_range,y=pos_line,group=1,color="Positive"))+
+    geom_line(aes(x=date_range,y=neg_line,group=1,color="Negative")) +geom_point(aes(x=date_range,y=neg_line,group=1,color="Negative"))+
+    geom_line(aes(x=date_range,y=neu_line,group=1,color="Neutral")) +geom_point(aes(x=date_range,y=neu_line,group=1,color="Neutral"))+
+    ggtitle(title_str)+labs(y="Number of Tweets",x="Date")+
+    scale_colour_manual(name="Sentiment",values=c("Positive"="#1E8449","Neutral"="#FFCC00","Negative"="red"))+
+    theme(legend.position="bottom",legend.direction='horizontal')+
+    theme(plot.title = element_text(hjust = 0.5))+theme(plot.title = element_text(lineheight=.8, face="bold"))
+}
+
+
+
+
+
+
+## This is a function that only extract the first n words of a string object. 
+string_fun <- function(x) {
+  ul = unlist(strsplit(x, split = "\\s+"))[1:8] #set n=8
+  paste(ul,collapse=" ")
+}
+
+
+
+
+
+
+##This function transforms the all_news database into a presentable DataTable
+news_transform<-function(db){
+  db$NEWS_DATE_NewsDim<- as.Date(db$NEWS_DATE_NewsDim,format='%d/%m/%Y')
+  db<- db[order(-as.numeric(db$NEWS_DATE_NewsDim)),] ##order by release dates, descending
+
+  db <- db[1:100,c('NEWS_TITLE_NewsDim','constituent','categorised_tag','sentiment')]
+
+  #make sure the news link only contains 8 characters from the headline. 
+  db$NEWS_TITLE_NewsDim <- as.character(db$NEWS_TITLE_NewsDim)
+  db$NEWS_TITLE_NewsDim <- unlist(lapply(db$NEWS_TITLE_NewsDim, string_fun)) ##Apply the limit
+  ##remove the NA
+  db$NEWS_TITLE_NewsDim <-gsub("NA", " ", db$NEWS_TITLE_NewsDim)
+
+
+  ##Assign the conditional sentiment values for conditioned-colors
+  index_posi<-db$sentiment=='positive' 
+  db$sentiment[index_posi]<-1
+  index_nega<-db$sentiment=='negative' 
+  db$sentiment[index_nega]<-1
+  index_neu<-db$sentiment=='neutral' 
+  db$sentiment[index_neu]=0
+
+  ##Fix the capital letters
+  db[db$constituent=='bmw',c('constituent')]='BMW'
+  db[db$constituent=='adidas',c('constituent')]='Adidas'
+  db[db$constituent=='commerzbank',c('constituent')]='Commerzbank'
+  db[db$constituent=='eon',c('constituent')]='EON'
+
+#db$Newslink<-paste('<a href="',db$Link,'">',db$Headline ,'</a>',sep="") #embed the hyperlink in headlines
+
+  df<- datatable(db[,c('NEWS_TITLE_NewsDim','constituent','sentiment')],rownames=FALSE, options = list(pageLength = 5),colnames = c('Headline' ,'Constituent','Sentiment'),escape=FALSE) %>%
+    formatStyle('sentiment',
+              color = styleInterval(c(-1,0),c('red','#FFCC00','#1E8449')),
+              backgroundColor = styleInterval(c(-1,0),c('red','#FFCC00','#1E8449')))
+}
+
+#This function creates a stacked bar for analyst recommendations. 
+analyst_stacked_bar<-function(retrieved_data){
+  df<- retrieved_data[,c('Constituent','% Buy','% Hold','% Sell')]
+  v<-(sort(df$Constituent,decreasing=FALSE))
+  df<-df[match(v,df$Constituent),]
+  dat.m <- melt(df,id.vars = "Constituent") 
+  dat.m$Percentage = dat.m$value*100
+  names(dat.m)[names(dat.m) == 'variable'] <- 'Key'
+  ggplot(dat.m,aes(x = dat.m$Constituent, y = Percentage,fill=Key,label=paste(Percentage,'%',sep='')))+
+    geom_bar(stat='identity')+coord_flip()+scale_fill_manual(values = c("#1E8449",'#FFCC00','red'))+
+    geom_text(size = 3, position = position_stack(vjust = 0.5))+ggtitle('Analyst Recommendations' )+
+    theme(plot.title = element_text(hjust = 0.5))+theme(plot.title = element_text(lineheight=.8, face="bold"))+
+    theme(legend.position="bottom",legend.direction='horizontal')+theme(plot.margin = unit(c(2,0,0,0), "cm"))
+}
+
+
+##This function takes the table from Mongodb and turns it into a color-coded summary datatable
+summary_box<-function(retrieved_data){
+  df<-retrieved_data[,c('constituent','twitter_sentiment','news_rating','profitability','risk')]
+  datatable(df,options=list(dom='t'),rownames = FALSE,colnames = c('Twitter Sentiment', 'News Rating','Profitability', 'Risk')) %>%
+  formatStyle(c('profitability','risk','twitter_sentiment','news_rating'),
+              color = styleInterval(c(-1,0),c('red','#FFCC00','#1E8449')),
+              backgroundColor = styleInterval(c(-1,0),c('red','#FFCC00','#1E8449')))}
---
+++ Dashboard_R_scripts/News_subfunctions.R
@@ -0,0 +1,89 @@
+##This stores functions used for the News Page of Analytics Dashboard.
+##Plot a word-cloud
+
+word_cloud_plot<-function(top_words_df,constituent){
+if(constituent=='Adidas'){
+  df<-top_words_df[top_words_df$Constituent==tolower(constituent),c('Words','Frequency')]}else{
+    df<-top_words_df[top_words_df$Constituent==(constituent),c('Words','Frequency')]}
+
+d <- tail(df[,c('Words','Frequency')])
+set.seed(1234)
+wordcloud(words = d$Words, freq = d$Frequency, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"))}
+
+
+##This plots a bar chart to display the number of news under any tags. 
+count_tags_bar<-function(news_df,constituent){
+title_str = paste('News Tagging Count of ',constituent, sep='')
+if (constituent=='Deutsche Bank'){
+  df<-news_df[news_df$constituent==(constituent),c('tag','count')]}else{
+    df<-news_df[news_df$constituent==tolower(constituent),c('tag','count')] }
+
+ggplot(df,aes(x=df$tag,y=df$count,fill=df$tag))+geom_col()+labs(y="Count of news",x="News tagging") +
+  geom_text(aes(label=df$count),hjust=0.5,vjust=0)+ggtitle(title_str) +scale_fill_discrete(guide=FALSE)+
+  theme(plot.title = element_text(hjust = 0.5))+theme(plot.title = element_text(lineheight=.8, face="bold"))
+}
+
+
+##This plots a line graph displaying the daily News Sentiment
+daily_news_sent<-function(url_mongo,constituent){
+  if(constituent=='Adidas'){
+    daily_news <- mongo(collection = 'news_daily_sent_adidas',
+                      url = url_mongo,
+                      verbose = FALSE, options = ssl_options())
+  }
+
+  if(constituent=='BMW'){
+   daily_news <- mongo(collection = 'news_daily_sent_bmw',
+                      url = url_mongo,
+                      verbose = FALSE, options = ssl_options())
+  }
+
+  if(constituent=='Commerzbank'){
+    daily_news <- mongo(collection = 'news_daily_sent_commerzbank',
+                      url = url_mongo,
+                      verbose = FALSE, options = ssl_options())
+  }
+  if(constituent=='Deutsche Bank'){  
+    daily_news <- mongo(collection = 'news_daily_sent_deutsche_bank',
+                      url = url_mongo,
+                      verbose = FALSE, options = ssl_options())}
+
+  if(constituent=='EON'){
+    daily_news <- mongo(collection = 'news_daily_sent_eon',
+                      url = url_mongo,
+                      verbose = FALSE, options = ssl_options())
+  }
+
+  df<-daily_news$find()
+  df$date <- as.Date(df$date,"%Y-%m-%d")
+
+  ggplot(data=df, aes(x=df$date, y=df$scorescore,group=1))+geom_line()+
+    geom_point()+
+    labs(y="Average news sentiment",x="Date")+
+    labs(title = title_str)+
+    ylim(-1,1)+
+    labs(title = paste("Daily News Sentiment for ",constituent,sep=""))+
+    theme(plot.title = element_text(hjust = 0.5))+theme(plot.title = element_text(lineheight=.8, face="bold"))
+    
+  }
+
+news_sent_by_tag<-function(news_tag_df,constituent){
+  if(constituent=='Deutsche Bank'){df<-news_tag_df[news_tag_df$constituent==constituent,]}else{
+    df<-news_tag_df[news_tag_df$constituent==tolower(constituent),]}
+  #df$date<-gsub("-", "/", df$date)
+  df$date <- as.Date(df$date,"%Y-%m-%d")
+  ##Only focus on shares, investment and stock related news
+  title_str = paste('Daily Average News Sentiment for ', constituent, ' by Categories',sep='')
+  #df_selected = df[(df$categorised_tag=='Stock'|df$categorised_tag=='Investment'|df$categorised_tag=='Shares'),]
+  ggplot(data = df,aes(x=df$date,y=df$scorescore,group=df$categorised_tag)) +
+    scale_y_continuous(limits=c(-1,1))+
+    geom_point(aes(color=categorised_tag))+geom_line(aes(color=categorised_tag))+
+    ggtitle(title_str)+labs(y="Daily average sentiment score",x="Date")+
+    scale_fill_discrete(name = "News Category")+
+    theme(legend.position="bottom",legend.direction='horizontal')+
+    labs(colour = "Category") +
+    theme(plot.title = element_text(hjust = 0.5))+theme(plot.title = element_text(lineheight=.8, face="bold"))
+}
+
