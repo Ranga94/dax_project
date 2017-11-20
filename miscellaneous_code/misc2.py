0582d895ecc1a5aa0d9402ecdf97e12ce9cfd3b4
ulince <4fe84.2Y>
2017-11-20 04:31:59 +0000

    Adding code to insert modified tweets

--- miscellaneous_code/misc2.py
+++ miscellaneous_code/misc2.py
@@ -1,5 +1,6 @@
 import sys
 import json
+from datetime import datetime
 
 def main(args):
 
@@ -8,6 +9,34 @@ def main(args):
             data = json.loads(line)
             data["news_topics"] = str(data["news_topics"])
             f2.write(json.dumps(data, cls=MongoEncoder) + '\n')
+
+def file_to_mongodb(args):
+    operations = []
+    storage = Storage(mongo_connection_string="mongodb://igenie_readwrite:igenie@35.189.89.82:27017/dax_gcp")
+    with open(args.file1, "r") as f1:
+        for line in f1:
+            data = json.loads(line)
+            #change column names
+            try:
+                data["NEWS_DATE_NewsDim"] = data.pop("news_date")
+                data["NEWS_TITLE_NewsDim"] = datetime.strptime(data.pop("news_title"), '%Y-%m-%d %H:%M:%S')
+                data["NEWS_ARTICLE_TXT_NewsDim"] = data.pop("news_article_txt")
+                data["NEWS_SOURCE_NewsDim"] = data.pop("news_source")
+                data["NEWS_PUBLICATION_NewsDim"] = data.pop("news_publication")
+                data["categorised_tag"] = data.pop("news_topics")
+            except Exception as e:
+                print(e)
+                continue
+
+            operations.append(data)
+
+            if len(operations) == 1000:
+                storage.save_to_mongodb(data, "dax_gcp", "all_news")
+                operations = []
+
+        if len(operations) > 0:
+            storage.save_to_mongodb(data, "dax_gcp", "all_news")
+
 
 if __name__ == "__main__":
     import argparse
@@ -18,4 +47,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     sys.path.insert(0, args.python_path)
     from utils.Storage import Storage, MongoEncoder
+
     main(args)
