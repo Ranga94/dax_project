1b0a225382f5000b3b1431b0ba064d3ffba13062
ulince <lince0r@gmail.com>
2017-11-26 14:29:27 +0000

    Adding PubSub code.

--- old_code/updating_all_tweets.py
+++ old_code/updating_all_tweets.py
@@ -1,11 +1,10 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 from pymongo import UpdateOne
 from pymongo import MongoClient
-from nltk.tag import StanfordNERTagger
-from nltk.tokenize import TweetTokenizer
 import sys
 import time
-from sner import Ner
+from datetime import datetime
+
 
 def main(args):
     client = MongoClient(args.connection_string)
@@ -13,50 +12,39 @@ def main(args):
     collection = db["tweets"]
 
     sia = SIA()
-    #st = StanfordNERTagger(argv[0],argv[1],encoding='utf-8')
-    tagger = Ner(host='localhost', port=9199)
-    tokenizer = TweetTokenizer(preserve_case=True, reduce_len=True, strip_handles=False)
+    tagger = TU()
 
     operations = []
-    cursor = collection.find({"constituent":"adidas", "nltk_sentiment_numeric":{"$exists":False}},{"_id":1,"text":1,"text_en":1,"semi_processed_text":1})
-    all_tweets = list(cursor)
     records = 0
 
     start_time = time.time()
 
-    for doc in all_tweets:
-        sentiment_score = get_nltk_sentiment(doc["semi_processed_text"],sia)
-        if "text_en" in doc:
-            tags = get_tags(doc["text_en"], tagger)
-        else:
-            tags = get_tags(doc["text"],tagger)
+    for doc in collection.find({}, no_cursor_timeout=True):
+        sentiment_score = get_nltk_sentiment(doc["text"],sia)
 
         new_values = {}
         new_values["nltk_sentiment_numeric"] = sentiment_score
-        new_values["tag_LOCATION"] = list()
-        new_values["tag_PERSON"] = list()
-        new_values["tag_ORGANIZATION"] = list()
-        new_values["tag_MONEY"] = list()
-        new_values["tag_PERCENT"] = list()
-        new_values["tag_DATE"] = list()
-        new_values["tag_TIME"] = list()
+        new_values["sentiment_score"] = sentiment_score
 
-        for word, tag in tags:
-            if tag != "O":
-                new_values["tag_" + tag].append(word)
+        if "relevance" not in doc:
+            new_values["relevance"] = -1
 
-        #print(new_values)
+        new_values['date'] = datetime.strptime(doc['created_at'], '%a %b %d %H:%M:%S %z %Y')
+
+        # TO DO
+        tagged_text = tagger.get_spacy_entities(doc["text"])
+        new_values["entity_tags"] = tap.get_spacey_tags(tagged_text)
 
         operations.append(
             UpdateOne({"_id": doc["_id"]}, {"$set": new_values})
         )
 
         # Send once every 1000 in batch
-        if (len(operations) == 1000):
+        if (len(operations) == 2000):
             print("Performing bulk write")
             collection.bulk_write(operations, ordered=False)
             operations = []
-            records += 1000
+            records += 2000
             print("Write done. Saved {} records".format(records))
 
     if (len(operations) > 0):
@@ -82,6 +70,10 @@ def get_tags(text, tagger):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
+    parser.add_argument('python_path', help='The connection string')
     parser.add_argument('connection_string', help='The connection string')
     args = parser.parse_args()
+    sys.path.insert(0, args.python_path)
+    from utils.TaggingUtils import TaggingUtils as TU
+    from utils import twitter_analytics_helpers as tap
     main(args)
