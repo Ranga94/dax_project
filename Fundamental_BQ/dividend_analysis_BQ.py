266dd9a5854bb0ff079e91c6d9acbe2c2558c6db
CuriousKomodo <hu.kefei@yahoo.co.uk>
2017-12-20 17:34:23 +0000

    Update dividend_analysis_BQ.py

--- Fundamental_BQ/dividend_analysis_BQ.py
+++ Fundamental_BQ/dividend_analysis_BQ.py
@@ -26,59 +26,65 @@ from google.cloud import bigquery
 ##This dividend table stores the results of linear regression, and the list of years when dividends are offered
 ##'Commerzbank','Deutsche Bank','Lufthansa','RWE','thyssenkrupp','Vonovia','Volkswagen (VW) vz'may not receive dividend yields
 
-#python dividend_analysis_BQ.py 'mysql+pymysql://igenie_readwrite:igenie@127.0.0.1/dax_project' 'PARAM_FINANCIAL_KEY_COLLECTION' 'igenie-project-key.json' 'pecten_dataset.dividend_t'
+#python dividend_analysis_BQ.py 'mysql+pymysql://igenie_readwrite:igenie@127.0.0.1/dax_project' 'PARAM_FINANCIAL_KEY_COLLECTION' 'igenie-project-key.json' 'pecten_dataset.dividend_analysis_t' 'test'
 
 
 def dividend_main(args):
     dividend_table = pd.DataFrame()
-    table_store = args.table_storage
-    project_name, constituent_list,table_store,table_master,table_historical = get_parameters(args)
-    table_store = args.table_storage
-    
-    for constituent in constituents_list:
-        master = get_master_data(project_name,table_master,constituent)
+    project_name, constituent_list,table_store,table_div,table_historical = get_parameters(args)
+    from_date, to_date = get_timerange(args)
+    date = datetime.strftime(datetime.now().date(),'%Y-%m-%d %H:%M:%S') 
+    #table_store = args.table_storage
+    #constituent_list = ['ProSiebenSat1 Media']
+    for constituent in constituent_list:
+        
+        #master = get_master_data(project_name,table_master,constituent)
         
         if constituent=='M\xc3\xbcnchener R\xc3\xbcckversicherungs-Gesellschaft':
             constituent = 'Münchener Rückversicherungs-Gesellschaft'
         elif constituent=='Deutsche B\xc3\xb6rse':
             constituent = 'Deutsche Börse'
             
-        div = get_dividend_data(args,constituent)
-        his = get_historical_price(project_name,table_historical,constituent)
+       
+        constituent_name = get_constituent_id_name(constituent)[1]
+        constituent_id = get_constituent_id_name(constituent)[0]  
+        
+        div =  get_dividend_data(project_name,table_div,constituent_id)
+        his = get_historical_price(project_name,table_historical,constituent,to_date)
+        
         current_price = float(his['closing_price'].iloc[0])
-        a,b,mse,year_list,current_div,future_div,score=dividend_analysis(div) #a represents the average growth of dividend per year in €
+        a,b,mse,current_div,future_div,score=dividend_analysis(div) #a represents the average growth of dividend per year in €
         if constituent == 'BMW' or 'Volkswagen (VW) vz' or 'RWE':
             a = a*2.0 #Calculate the annual dividend growth for stocks that release dividend every 6 months
-        if constituent != 'ProSiebenSat1 Media':
-            estimated_return = Gordon_Growth_estimated_return(a,current_price,current_div,future_div)
-        else:
+        #if constituent != 'ProSiebenSat1 Media':
+        print current_div
+        print future_div
+        if future_div == 'n/a':
             estimated_return = 0
+        else:
+            estimated_return = Gordon_Growth_estimated_return(a,current_price,current_div,future_div)
+        div_yield = 1.0*current_div/current_price
         
-        dividend_table = dividend_table.append(pd.DataFrame({'Constituent': constituent, 'Current dividend': current_div, 'Average rate of dividend growth /year':round(a,2),'Mean square error of fitting': round(mse,2),'Years of dividend offer':'%s'%year_list,'Estimated dividend next year':future_div,'Current share price':current_price,'Gordon growth estimated return':estimated_return,'Dividend consistency score':score,'Table':'dividend analysis','Status':"active",'Date':str(datetime.date.today())}, index=[0]), ignore_index=True)
+        dividend_table = dividend_table.append(pd.DataFrame({'Constituent': constituent, 'Constituent_name':constituent_name,'Constituent_id':constituent_id,'Current_dividend': current_div,'Current_dividend_yield':div_yield, 'Average_rate_of_dividend_growth_per_year':round(a,2),'Mean_square_error_of_fitting': round(mse,2),'Estimated_dividend_next_year':future_div,'Current_share_price':current_price,'Gordon_growth_estimated_return':estimated_return,'Dividend_consistency_score':score,'Table':'dividend analysis','Status':"active",'Date_of_analysis':date,'From_date':from_date,"To_date":to_date}, index=[0]), ignore_index=True)
     
-    status_update(args)
-    store_result(args, dividend_table)
+    update_result(table_store)
+    store_result(args,project_name,table_store,dividend_table)
+    
 
 #Computes the linear regression model for dividend, and produce list of years where dividend is offered. 
 def dividend_analysis(div):
-    div = div[['Dividend','year']].dropna(thresh=1)
-    div = pd.DataFrame(div)
-    value = [unicode(x) for x in div["Value"]]
-    value = [x.replace(u'\u20ac',"") for x in value]
-    value = [float(x) for x in value]
-    n=len(value)
-    current_div = value[0] 
+    div = div[['Value','Last_Dividend_Payment']].dropna(thresh=1)
+    n=len(div)
+    current_div = div['Value'].iloc[-1] 
     
     #Estimate growth of dividend using linear fitting
-    z = np.polyfit(range(n),value[::-1],1)
+    z = np.polyfit(range(n),np.asarray(div['Value']),1)
     estimation = [x*z[0]+z[1] for x in range(n)]
-    res = map(operator.sub, value[::-1], estimation)
+    res = map(operator.sub, np.asarray(div['Value']), estimation)
     mse = sum([x**2 for x in res])/n*1.0
     
     ##Find out the years where dividend is offered
     #Volkswagen (VW) vz, BMW, RWE: half year
-    date_list = pd.DatetimeIndex(div['year'])
-    year_list = date_list.year
     
     score =0 #This scores the consistency of dividend issued each year. 
     if mse<0.5:
@@ -88,7 +94,7 @@ def dividend_analysis(div):
         future_div ='n/a'
         score = score+0
     
-    return z[0],z[1],mse,year_list,current_div,future_div,score
+    return z[0],z[1],mse,current_div,future_div,score
 
 
 def Gordon_Growth_estimated_return(div_growth,current_price,current_div,future_div):
@@ -96,11 +102,18 @@ def Gordon_Growth_estimated_return(div_growth,current_price,current_div,future_d
     return estimated_return
 
 
+def get_timerange(args):
+    query = 'SELECT * FROM PARAM_READ_DATE WHERE STATUS = "active";'
+    timetable = pd.read_sql(query, con=args.sql_connection_string)
+    from_date = timetable['FROM_DATE'].loc[timetable['ENVIRONMENT']=='test']
+    to_date = timetable['TO_DATE'].loc[timetable['ENVIRONMENT']=='test']
+    return from_date[0], to_date[0]
+    
 
 #this obtains the historical price data as a pandas dataframe from source for one constituent. 
-def get_historical_price(project_name,table_historical,constituent):
+def get_historical_price(project_name,table_historical,constituent,to_date):
     #Obtain project name, table for historical data in MySQL
-    QUERY ='SELECT closing_price,date FROM '+ table_historical + ' WHERE Constituent= "'+constituent+'"'+ " AND date between TIMESTAMP ('2008-01-01 00:00:00 UTC') and TIMESTAMP ('2017-12-11 00:00:00 UTC') ;"
+    QUERY ='SELECT closing_price,date FROM '+ table_historical + ' WHERE Constituent= "'+constituent+'"'+ " AND date between TIMESTAMP ('2017-01-01 00:00:00 UTC') and TIMESTAMP ('" + str(to_date) + " UTC') ;"
     print QUERY
     his=pd.read_gbq(QUERY, project_id=project_name)
     his['date'] = pd.to_datetime(his['date'],format="%Y-%m-%dT%H:%M:%S") #read the date format
@@ -109,10 +122,9 @@ def get_historical_price(project_name,table_historical,constituent):
     
 
 
-
 def get_parameters(args):
     script = 'dividend_analysis'
-    query = 'SELECT * FROM'+' '+ args.parameter_table + ';'
+    query = 'SELECT * FROM'+' '+ args.parameter_table + ' WHERE ENVIRONMENT ="' +args.environment+ '";'
     print query
     parameter_table = pd.read_sql(query, con=args.sql_connection_string)
     project_name = parameter_table["PROJECT_NAME_BQ"].loc[parameter_table['SCRIPT_NAME']==script].values[0]
@@ -122,16 +134,16 @@ def get_parameters(args):
     constituent_list=np.asarray(ast.literal_eval((a.values[0])))
     
     #Obtain the table storing historical price
-    table_master = parameter_table["TABLE_COLLECT_FUNDAMENTAL_BQ"].loc[parameter_table['SCRIPT_NAME']==script].values[0]
-    table_master = 'pecten_dataset.historical_key_data'
+    table_div = parameter_table["TABLE_COLLECT_FUNDAMENTAL_BQ"].loc[parameter_table['SCRIPT_NAME']==script].values[0]
+    table_div = 'pecten_dataset.dividend' #Overwrite temporarily
     table_historical = parameter_table["TABLE_COLLECT_HISTORICAL_BQ"].loc[parameter_table['SCRIPT_NAME']==script].values[0]
-    table_store = parameter_table['TABLE_STORE_ANALYSIS_BQ'].loc[parameter_table['SCRIPT_NAME']==script].values[0]
-    return project_name, constituent_list,table_store,table_master,table_historical
+    table_store = parameter_table['TABLE_STORE_ANALYSIS_BQ'].loc[parameter_table['SCRIPT_NAME']==script].values[0] 
+    return project_name, constituent_list,table_store,table_div,table_historical
 
 
 
 def update_result(table_store):
-    storage = Storage(google_key_path='igenie-project-key.json' )
+    storage = Storage(google_key_path=args.service_key_path )
     query = 'UPDATE `' + table_store +'` SET Status = "inactive" WHERE Status = "active"'
 
     try:
@@ -141,14 +153,14 @@ def update_result(table_store):
 
 
 #this obtains the master data as a pandas dataframe from source for one constituent. 
-def get_dividend_data(project_name,table_master,constituent):
-    QUERY ='SELECT * FROM '+ table_master + ' WHERE Constituent= "'+constituent+'";'
+def get_dividend_data(project_name,table_div,constituent_id):
+    QUERY ='SELECT * FROM '+ table_div + ' WHERE constituent_id= "'+constituent_id+'";'
     print QUERY
-    master=pd.read_gbq(QUERY, project_id=project_name)
-    master['year'] = pd.to_datetime(master['year'],format="%Y-%m-%dT%H:%M:%S") #read the date format
-    div_data = master.sort_values('year',ascending=1).reset_index(drop=True) 
-    
-    return div_data
+    div=pd.read_gbq(QUERY, project_id=project_name)
+    div = div[div['Dividend_Cycle']=='Annually']
+    div['Last_Dividend_Payment'] = pd.to_datetime(div['Last_Dividend_Payment'],format="%Y-%m-%d") #read the date format
+    div = div.sort_values('Last_Dividend_Payment',ascending=1).reset_index(drop=True)  #Sorting from the earliest to the latest
+    return div
 
 
 def store_result(args,project_name,table_store,result_df):
@@ -221,7 +233,7 @@ def get_constituent_id_name(old_constituent_name):
     mapping["Vonovia"] = ("VNADE5050438829" , "VONOVIA SE")
     mapping["DAX"] = ("DAX", "DAX")
     mapping["Fresenius Medical Care"] = ("FMEDE8110066557" , "FRESENIUS MEDICAL CARE AG & CO.KGAA")
-    mapping["Volkswagen"] = ("VOW3DE2070000543" , "VOLKSWAGEN AG")
+    mapping["Volkswagen (VW) vz"] = ("VOW3DE2070000543" , "VOLKSWAGEN AG")
     mapping["Münchener Rückversicherungs-Gesellschaft"] = ("MUV2DEFEI1007130" , "MUNCHENER RUCKVERSICHERUNGS - GESELLSCHAFT AKTIENGESELLSCHAFT IN MUNCHEN")
 
     if old_constituent_name in mapping:
@@ -237,7 +249,7 @@ if __name__ == "__main__":
     parser.add_argument('parameter_table',help="The name of the parameter table in MySQL")
     parser.add_argument('service_key_path',help='google service key path')
     parser.add_argument('table_storage',help='BigQuery table where the new data is stored')
-    
+    parser.add_argument('environment',help = 'test or production')
     args = parser.parse_args()
     
     
