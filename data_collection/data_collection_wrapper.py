c273490d587c85508101f9d1eb63207b7e1f0395
ulince <lince0r@gmail.com>
2018-01-03 15:24:59 +0000

    Fixed language code bug.

--- data_collection/data_collection_wrapper.py
+++ data_collection/data_collection_wrapper.py
@@ -6,44 +6,57 @@ def main(args):
     print("Collection logs: {}".format(str(datetime.now())))
     try:
         print("Executing news_collection_orbis")
-        news_collection_orbis.main(args)
+        #news_collection_orbis.main(args)
     except Exception as e:
         print(e)
     try:
         print("Executing historical_scraper")
         args_1 = deepcopy(args)
         args_1.all = True
-        historical_scraper.main(args_1)
+        #historical_scraper.main(args_1)
     except Exception as e:
         print(e)
     try:
         print("Executing news_collection_zephyr")
-        news_collection_zephyr.main(args)
+        #news_collection_zephyr.main(args)
     except Exception as e:
         print(e)
     try:
         print("Executing yahoo_finance")
-        yahoo_finance.main(args)
+        #yahoo_finance.main(args)
     except Exception as e:
         print(e)
     try:
         print("Executing reuters_scraper")
-        reuters_scraper.main(args)
+        #reuters_scraper.main(args)
     except Exception as e:
         print(e)
     try:
         print("Executing stocktwits")
-        stocktwits.main(args)
+        #stocktwits.main(args)
     except Exception as e:
         print(e)
     try:
         print("Executing bloomberg_scraper")
-        bloomberg_scraper.main(args)
+        #bloomberg_scraper.main(args)
     except Exception as e:
         print(e)
     try:
         print("Executing ticker_data")
-        ticker_data.main(args)
+        #ticker_data.main(args)
+    except Exception as e:
+        print(e)
+    try:
+        print("Executing analyst opinions web scraping")
+        args_2 = deepcopy(args)
+        args_2.sql_connection_string = args.param_connection_string
+        args_2.service_key_path = args.google_key_path
+        args_2.display_parameter = 1
+        if args.environment == 'test':
+            args_2.table_storage = 'pecten_dataset_{}.{}'.format(args.environment, args.analyst_opinions_table)
+        else:
+            args_2.table_storage = 'pecten_dataset.{}'.format(args.analyst_opinions_table)
+        webscraping_analyst_BQ.main(args)
     except Exception as e:
         print(e)
     try:
@@ -59,6 +72,7 @@ if __name__ == "__main__":
     parser.add_argument('google_key_path', help='The path of the Google key')
     parser.add_argument('param_connection_string', help='The connection string')
     parser.add_argument('environment', help='production or test')
+    parser.add_argument('analyst_opinions_table')
     args = parser.parse_args()
     sys.path.insert(0, args.python_path)
     from data_collection.twitter import twitter_collection as twitter_collection
@@ -70,4 +84,5 @@ if __name__ == "__main__":
     from data_collection.news import reuters_scraper as reuters_scraper
     from data_collection.rss_feeds import yahoo_finance as yahoo_finance
     from data_collection.stocktwits_collection import stocktwits as stocktwits
+    from data_collection.analyst_rating_webscraping import webscraping_analyst_BQ as webscraping_analyst_BQ
     main(args)
