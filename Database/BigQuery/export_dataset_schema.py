2dd9d87d423a10dae67a3e29571b51477694ab78
ulince <lince0r@gmail.com>
2018-01-04 18:52:31 +0000

    Bug number: CH_DB_1 Bug type: Change, databse Adding file copy_missing_objects.py, which calls export_dataset_schema.py to export the schema of tables in a dataset and imports the missing ones to  destination dataset.

--- Database/BigQuery/export_dataset_schema.py
+++ Database/BigQuery/export_dataset_schema.py
@@ -3,45 +3,21 @@ from google.cloud import bigquery
 import sys
 import os
 
-def main(args):
+def export_schema(args):
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = args.google_key_path
     client = bigquery.Client()
-    dataset_ref = client.dataset('pecten_dataset')
-    dataset = client.get_dataset(dataset_ref)
-    tables = list(client.list_dataset_tables(dataset))
-    table_ref = dataset.table(tables[0].table_id)
-    print(table_exists(client, table_ref))
-
-    source_dataset = bigquery.DatasetReference('bigquery-public-data', 'samples')
-    source_table_ref = source_dataset.table('shakespeare')
-    dest_dataset = bigquery.Dataset(client.dataset(DATASET_ID))
-    dest_dataset = client.create_dataset(dest_dataset)
-    dest_table_ref = dest_dataset.table('destination_table')
-
-def table_exists(client, table_reference):
-    """Return if a table exists.
-
-    Args:
-        client (google.cloud.bigquery.client.Client):
-            A client to connect to the BigQuery API.
-        table_reference (google.cloud.bigquery.table.TableReference):
-            A reference to the table to look for.
-
-    Returns:
-        bool: ``True`` if the table exists, ``False`` otherwise.
-    """
-    from google.cloud.exceptions import NotFound
-
-    try:
-        client.get_table(table_reference)
-        return True
-    except NotFound:
-        return False
+    source_dataset_ref = client.dataset(args.source_dataset)
+    source_dataset = client.get_dataset(source_dataset_ref)
+    return list(client.list_dataset_tables(source_dataset))
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
+    parser.add_argument('python_path', help='The connection string')
     parser.add_argument('google_key_path', help='The connection string')
-    parser.add_argument('google_key_path', help='The connection string')
+    parser.add_argument('source_dataset')
+    args = parser.parse_args()
+    sys.path.insert(0, args.python_path)
+    export_schema(args)
 
 
