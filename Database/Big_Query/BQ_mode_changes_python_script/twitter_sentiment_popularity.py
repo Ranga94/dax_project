cadba8a92d216f8b19dc8a47fae5ed8cb7cc349e
Ranga <rangavittalprasad@gmail.com>
2018-01-15 01:54:20 +0530

    code changes

--- Database/Big_Query/BQ_mode_changes_python_script/twitter_sentiment_popularity.py
+++ Database/Big_Query/BQ_mode_changes_python_script/twitter_sentiment_popularity.py
@@ -35,7 +35,7 @@ def load_data_from_gcs(dataset_id, table_id, source):
 	table_ref = dataset_ref.table(table_id)
 	job_config = bigquery.LoadJobConfig()
 	job_config.source_format = 'NEWLINE_DELIMITED_JSON'
-	job_config.maxBadRecords=10
+	job_config.maxBadRecords=100
 	job = bigquery_client.load_table_from_uri(source, table_ref,job_config=job_config)
 	
 	job.result()  # Waits for job to complete
