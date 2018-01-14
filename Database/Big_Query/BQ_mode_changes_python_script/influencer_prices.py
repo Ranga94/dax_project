4bb47a1991b57e9860cafb4e66c90a25b062c344
Ranga <rangavittalprasad@gmail.com>
2018-01-15 00:04:22 +0530

    code

--- Database/Big_Query/BQ_mode_changes_python_script/influencer_prices.py
+++ Database/Big_Query/BQ_mode_changes_python_script/influencer_prices.py
@@ -24,10 +24,9 @@ def create_table(dataset_id, table_id, project):
 	bigquery.SchemaField('constituent_id','STRING','REQUIRED'),
 	bigquery.SchemaField('date','TIMESTAMP','NULLABLE'),
     )
+    table = bigquery_client.create_table(table)
 
-	table = bigquery_client.create_table(table)
-
-	print('Created table {} in dataset {}.'.format(table_id, dataset_id))
+    print('Created table {} in dataset {}.'.format(table_id, dataset_id))
 	
 def load_data_from_gcs(dataset_id, table_id, source):
 	bigquery_client = bigquery.Client()
