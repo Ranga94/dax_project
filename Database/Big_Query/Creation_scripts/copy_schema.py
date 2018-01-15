c81ae0f35e4f22b1b678e435b1257ae833f64114
Ranga <rangavittalprasad@gmail.com>
2018-01-15 17:38:47 +0530

    code

---
+++ Database/Big_Query/Creation_scripts/copy_schema.py
@@ -0,0 +1,51 @@
+from google.cloud import bigquery
+import sys
+import os
+
+def copy_missing_objects(args):
+    client = bigquery.Client()
+    source_dataset_ref = client.dataset(args.source_dataset)
+    source_dataset = client.get_dataset(source_dataset_ref)
+    tables = export_schema(args)
+
+    destination_dataset_ref = client.dataset(args.destination_dataset)
+    destination_dataset = client.get_dataset(destination_dataset_ref)
+
+    for table in tables:
+        source_table_ref = source_dataset.table(table.table_id)
+        destination_table_ref = destination_dataset.table(table.table_id)
+    if not table_exists(client, destination_table_ref):
+        print("Creating table {} in {}".format(table.table_id, args.destination_dataset))
+        #Get schema of source table
+        source_table = client.get_table(source_table_ref)
+        schema = source_table.schema
+        #Create empty table on the other dataset
+        destination_table = bigquery.Table(destination_table_ref, schema=schema)
+        destination_table = client.create_table(destination_table)
+
+def table_exists(client, table_reference):
+    """Return if a table exists.
+    Args:
+        client (google.cloud.bigquery.client.Client):
+            A client to connect to the BigQuery API.
+        table_reference (google.cloud.bigquery.table.TableReference):
+            A reference to the table to look for.
+    Returns:
+        bool: ``True`` if the table exists, ``False`` otherwise.
+    """
+    from google.cloud.exceptions import NotFound
+
+    try:
+        client.get_table(table_reference)
+        return True
+    except NotFound:
+        return False
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_dataset')
+    parser.add_argument('destination_dataset')
+    args = parser.parse_args()
+    from .export_dataset_schema import export_schema
+    copy_missing_objects(args)
