e9ad2ef8daf06d2c23324380cc7b29a5c1fd5f45
Ranga <rangavittalprasad@gmail.com>
2018-01-15 18:54:21 +0530

    code changes

--- Database/Big_Query/Creation_scripts/copy_schema.py
+++ Database/Big_Query/Creation_scripts/copy_schema.py
@@ -45,8 +45,8 @@ def table_exists(client, table_reference):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('python_path', help='The connection string')
     parser.add_argument('google_key_path', help='The connection string')
+	parser.add_argument('python_path', help='The connection string')
     parser.add_argument('source_dataset')
     parser.add_argument('destination_dataset')
     args = parser.parse_args()
