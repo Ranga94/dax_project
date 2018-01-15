b94239550e5ac25bb130ec4d09e31bb15553ff86
Ranga <rangavittalprasad@gmail.com>
2018-01-15 17:05:31 +0530

    code

---
+++ Database/Big_Query/Creation_scripts/technical_figures.py
@@ -0,0 +1,37 @@
+from google.cloud import bigquery
+import sys
+
+def create_table(dataset_id, table_id, project):
+    """Creates a simple table in the given dataset.
+
+    If no project is specified, then the currently active project is used.
+    """
+    bigquery_client = bigquery.Client(project=project)
+    dataset_ref = bigquery_client.dataset(dataset_id)
+
+    table_ref = dataset_ref.table(table_id)
+    table = bigquery.Table(table_ref)
+
+    # Set the table schema
+    table.schema = (
+	bigquery.SchemaField('constituent_name','STRING','NULLABLE'),
+	bigquery.SchemaField('constituent_id','STRING','NULLABLE'),
+	bigquery.SchemaField('date_of_collection','TIMESTAMP','NULLABLE'),
+	bigquery.SchemaField('Alpha_250_days','FLOAT','NULLABLE'),
+	bigquery.SchemaField('Alpha_30_days','FLOAT','NULLABLE'),
+	bigquery.SchemaField('Beta_30_days','FLOAT','NULLABLE'),
+	bigquery.SchemaField('Beta_250_days','FLOAT','NULLABLE'),
+	bigquery.SchemaField('Correlation_30_days','FLOAT','NULLABLE'),
+	bigquery.SchemaField('Correlation_250_days','FLOAT','NULLABLE'),
+	bigquery.SchemaField('Volatility_60_days','FLOAT','NULLABLE'),
+		
+    )
+
+    table = bigquery_client.create_table(table)
+
+    print('Created table {} in dataset {}.'.format(table_id, dataset_id))
+	#bigquery.SchemaField('','',''),		 
+
+
+if __name__ == '__main__':
+	create_table(sys.argv[1],sys.argv[2],"igenie-project")
