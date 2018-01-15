4bb271417b1b0808c0d19fd0a583867bbae867a7
Ranga <rangavittalprasad@gmail.com>
2018-01-15 17:44:20 +0530

    code

---
+++ Database/Big_Query/Creation_scripts/export_dataset_schema.py
@@ -0,0 +1,18 @@
+from google.cloud import bigquery
+import sys
+import os
+
+def export_schema(args):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = args.google_key_path
+    client = bigquery.Client()
+    source_dataset_ref = client.dataset(args.source_dataset)
+    source_dataset = client.get_dataset(source_dataset_ref)
+    return list(client.list_dataset_tables(source_dataset))
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_dataset')
+    args = parser.parse_args()
+    sys.path.insert(0, args.python_path)
+    export_schema(args)
