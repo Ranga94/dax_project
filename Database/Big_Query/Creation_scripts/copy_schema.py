8bda47259d7d1e78c69037cab45330d46a07c117
Ranga <rangavittalprasad@gmail.com>
2018-01-15 18:24:54 +0530

    code

--- Database/Big_Query/Creation_scripts/copy_schema.py
+++ Database/Big_Query/Creation_scripts/copy_schema.py
@@ -48,6 +48,6 @@ if __name__ == "__main__":
     parser.add_argument('source_dataset')
     parser.add_argument('destination_dataset')
     args = parser.parse_args()
-	sys.path.insert(0, args.python_path)
+    sys.path.insert(0, args.python_path)
     from .export_dataset_schema import export_schema
     copy_missing_objects(args)
