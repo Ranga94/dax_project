711d7344f6b086cfc9e7a0da63cd47274338a1e7
Ranga <rangavittalprasad@gmail.com>
2018-01-17 16:04:00 +0530

    code changes

--- Database/Big_Query/Creation_scripts/news_logs.py
+++ Database/Big_Query/Creation_scripts/news_logs.py
@@ -1,12 +1,13 @@
 from google.cloud import bigquery
+import os 
 
 def news_log_read():
+	os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "E:\igenie-project-key.json"
 	client = bigquery.Client()
-	query_job = client.query("""
-		SELECT count(*) as unique_words FROM pecten_dataset_test.news_logs where date = '2018-01-17'"""
+	query_job = client.query("SELECT count(*) as unique_words FROM pecten_dataset_test.news_logs where date = '2018-01-17'")
 		
 	results = query_job.result()
-	for row in results
+	for row in results:
 		print("{}".format(row.unique_words))
 		
 if __name__ == '__main__':
