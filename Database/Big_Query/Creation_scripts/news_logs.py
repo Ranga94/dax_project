4b48c861b38346766dadfac4f1f6d8d49f6c6110
Ranga <rangavittalprasad@gmail.com>
2018-01-17 21:45:08 +0530

    messages

--- Database/Big_Query/Creation_scripts/news_logs.py
+++ Database/Big_Query/Creation_scripts/news_logs.py
@@ -17,7 +17,7 @@ def news_log_read():
 	s = ""
 	#constituent_name = []
 	for row in results:
-		s + = ("{} news items were inserted for {}"+"\n".format(row.number,row.constituent_name))
+		s += ("{} news items were inserted for {}"+"\n".format(row.number,row.constituent_name))
 	#	constituent_name.append(row.constituent_name)
 	#for i, j in zip(number, constituent_name):
 	#	msg = "{} news items were inserted for {} \n".format(i,j)
