572b5eda8760fd26b83f4eeaac5d6056fe62d617
Ranga <rangavittalprasad@gmail.com>
2018-01-17 22:09:21 +0530

    messages

--- Database/Big_Query/Creation_scripts/news_logs.py
+++ Database/Big_Query/Creation_scripts/news_logs.py
@@ -9,7 +9,7 @@ import pandas as pd
 def news_log_read():
 	today = DT.date.today()
 	week_ago = today - DT.timedelta(days=7)
-	print("News collection report for the date range between {} and {}:".format(week_ago, today))
+	message = ("News collection report for the date range between" + week_ago + "and" +today+":")
 	client = bigquery.Client()
 	query_job = client.query("SELECT constituent_name, count(*) as number FROM pecten_dataset_test.news_logs where date between TIMESTAMP'{}' and TIMESTAMP '{}' GROUP BY constituent_name ORDER BY number".format(week_ago, today))
 		
@@ -17,14 +17,15 @@ def news_log_read():
 	s = ""
 	#constituent_name = []
 	for row in results:
-		s = s+str(row.number)+" news items were inserted for "+row.constituent_name+"\n"
-	print(s)	
+		s = s + row.constituent_name : row.number
+		#s = s+str(row.number)+" news items were inserted for "+row.constituent_name+"\n"
+	message = message + "\n" + s	
 	
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.starttls()
 	server.login("rangavittal2@gmail.com", sys.argv[1])
 	toaddrs = [sys.argv[2]]
-	server.sendmail("rangavittal2@gmail", toaddrs, s)
+	server.sendmail("rangavittal2@gmail", toaddrs, message)
 	server.quit()
 		
 if __name__ == '__main__':
