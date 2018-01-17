f204a255a5bf83626fa7ae97b607bb348f9d44fc
Ranga <rangavittalprasad@gmail.com>
2018-01-17 16:53:08 +0530

    messages

--- Database/Big_Query/Creation_scripts/news_logs.py
+++ Database/Big_Query/Creation_scripts/news_logs.py
@@ -4,8 +4,10 @@ import os
 
 def news_log_read():
 	#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "E:\igenie-project-key.json"
+	now = datetime.datetime.now()
+	date = now.strftime("%Y-%m-%d")
 	client = bigquery.Client()
-	query_job = client.query("SELECT constituent_name, count(*) as unique_words FROM pecten_dataset_test.news_logs where date = '2018-01-17' GROUP BY constituent_name")
+	query_job = client.query("SELECT constituent_name, count(*) as unique_words FROM pecten_dataset_test.news_logs where date = '{}' GROUP BY constituent_name".format(date))
 		
 	results = query_job.result()
 	for row in results:
