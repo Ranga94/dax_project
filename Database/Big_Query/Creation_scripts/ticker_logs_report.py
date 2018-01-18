48c0d99159c4904d64d753736703b0176f918302
Ranga <rangavittalprasad@gmail.com>
2018-01-18 12:58:31 +0530

    messages

---
+++ Database/Big_Query/Creation_scripts/ticker_logs_report.py
@@ -0,0 +1,31 @@
+from google.cloud import bigquery
+import datetime as DT
+import sys
+import smtplib
+
+
+def ticker_log():
+	today = DT.date.today()
+	week_ago = today - DT.timedelta(days=7)
+	subject = ("News collection report for the date range between " + str(week_ago) + " and " +str(today)+":")
+	client = bigquery.Client()
+	query_job = client.query("SELECT constituent_name, sum(downloaded_ticks) as number FROM `igenie-project.pecten_dataset_new.ticker_logs` where date BETWEEN TIMESTAMP '{}' and TIMESTAMP'{}' GROUP BY constituent_name ORDER BY number".format(week_ago, today))
+		
+	results = query_job.result()
+	body = ""
+	#constituent_name = []
+	for row in results:
+		body = body + row.constituent_name +": " + str(row.number) + "\n"
+		#s = s+str(row.number)+" news items were inserted for "+row.constituent_name+"\n"
+	#message = message + "\n" + s
+	message = 'Subject: {}\n\n{}'.format(subject, body)	
+	print(message)
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login("rangavittal2@gmail.com", sys.argv[1])
+	toaddrs = [sys.argv[2]]
+	server.sendmail("rangavittal2@gmail", toaddrs, message)
+	server.quit()
+		
+if __name__ == '__main__':
+	ticker_log
