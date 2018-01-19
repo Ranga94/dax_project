15a81aa8204ba2ee860863c8c53ca8cd85060992
Ranga <rangavittalprasad@gmail.com>
2018-01-19 13:06:10 +0530

    code changes

--- Database/Big_Query/Creation_scripts/rss_feeds.py
+++ Database/Big_Query/Creation_scripts/rss_feeds.py
@@ -8,13 +8,13 @@ def rss_feeds():
 	week_ago = today - DT.timedelta(days=7)
 	subject = ("RSS feeds collection report for the date range between " + str(week_ago) + " and " + str(today) + ":")
 	client = bigquery.Client()
-	query_job = client.query("SELECT constituent, count(*) as number FROM `igenie-project.pecten_dataset_test.all_news` where news_origin = 'Yahoo Finance RSS' and news_date BETWEEN TIMESTAMP '{}' and TIMESTAMP'{}' GROUP BY constituent ORDER BY number".format(week_ago, today))
+	query_job = client.query("SELECT constituent_name, count(*) as number FROM `igenie-project.pecten_dataset_test.all_news` where news_origin = 'Yahoo Finance RSS' and news_date BETWEEN TIMESTAMP '{}' and TIMESTAMP'{}' GROUP BY constituent_name ORDER BY number".format(week_ago, today))
 	
 	results = query_job.result()
 	body = ""
 	#constituent_name = []
 	for row in results:
-		body = body + row.constituent +": " + str(row.number) + "\n"
+		body = body + row.constituent_name +": " + str(row.number) + "\n"
 		#s = s+str(row.number)+" news items were inserted for "+row.constituent_name+"\n"
 	#message = message + "\n" + s
 	message = 'Subject: {}\n\n{}'.format(subject, body)	
