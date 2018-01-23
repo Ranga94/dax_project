1bdc5b27b45dbd97427045f519539f1344ee098c
Ranga <rangavittalprasad@gmail.com>
2018-01-23 11:52:18 +0530

    master_log

--- Database/Big_Query/Creation_scripts/master_log_table_query.py
+++ Database/Big_Query/Creation_scripts/master_log_table_query.py
@@ -47,8 +47,21 @@ def log_table():
 	orbis_df = pd.DataFrame({'Constituent_name':constituent_name2,
 								'orbis':orbis})
 	collect1 = pd.merge(collect,orbis_df,on='Constituent_name',how='left')
+	########################################rss_feeds######################
+	query_rss = client.query("""SELECT constituent_name, sum(downloaded_news) as orbis FROM `igenie-project.pecten_dataset_test.news_logs` 
+	where date = TIMESTAMP('{}') and source = 'Yahoo Finance RSS'
+	GROUP BY constituent_name""".format(day_before))
+	rss_results = query_rss.result()
+	constituent_name3 = []
+	rss = []
+	for row in rss_results:
+		constituent_name3.append(row.constituent_name)
+		rss.append(row.rss)
+	rss_df = pd.DataFrame({'Constituent_name':constituent_name3,
+								'rss':rss})
+	collect2 = pd.merge(collect1,rss_df,on='Constituent_name',how='left')
 	########################################Ticker log Query ###############################
-	query_ticker = client.query("""SELECT constituent_name, sum(downloaded_ticks) as ticks  FROM `igenie-project.pecten_dataset_test.ticker_logs`
+	"""query_ticker = client.query("""SELECT constituent_name, sum(downloaded_ticks) as ticks  FROM `igenie-project.pecten_dataset_test.ticker_logs`
 	where date = TIMESTAMP('{}')
 	GROUP BY constituent_name""".format(day_before))
 	ticker_results = query_ticker.result()
@@ -60,7 +73,7 @@ def log_table():
 	ticker_df = pd.DataFrame({'Constituent_name':constituent_name2,
 								'ticker':ticker})
 	collect2 = pd.merge(collect1, ticker_df, on = 'Constituent_name', how ='left')
-	print(collect2)
+	print(collect2)"""
 if __name__ == '__main__':
 	log_table()
 	
