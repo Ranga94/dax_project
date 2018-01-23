db1116d239943597e0463595d2919855728641fb
Ranga <rangavittalprasad@gmail.com>
2018-01-23 11:15:54 +0530

    master_log

--- Database/Big_Query/Creation_scripts/master_log_table_query.py
+++ Database/Big_Query/Creation_scripts/master_log_table_query.py
@@ -32,9 +32,22 @@ def log_table():
 	bloomberg_df = pd.DataFrame({'Constituent_name':constituent_name1,
 								'bloomberg':bloomberg})
 	
-	collect2 = pd.merge(tweets_df,bloomberg_df,on ='Constituent_name',how='left')
-	print(collect2)
-	
+	#collect2 = pd.merge(tweets_df,bloomberg_df,on ='Constituent_name',how='left')
+	#print(collect2)
+	##########################################Orbis Log Query################################
+	query_orbis = client.query("""SELECT constituent_name, sum(downloaded_news) as bloomberg FROM `igenie-project.pecten_dataset_test.news_logs` 
+	where date = TIMESTAMP('{}') and source = 'Orbis'
+	GROUP BY constituent_name""".format(day_before))
+	orbis_results = query_orbis.result()
+	constituent_name2 = []
+	orbis = []
+	for row in bloomberg_results:
+		constituent_name2.append(row.constituent_name)
+		orbis.append(row.orbis)
+	orbis_df = pd.DataFrame({'Constituent_name':constituent_name2,
+								'orbis':orbis})
+	collect = pd.merge(tweets_df,bloomberg_df,orbis_df,on='Constituent_name',how='left')
+	print(collect)
 if __name__ == '__main__':
 	log_table()
 	
