f74f944c41556557a882d0ba18e1ac1659cbe867
Ranga <rangavittalprasad@gmail.com>
2018-01-24 13:54:15 +0530

    master_log

--- Database/Big_Query/Creation_scripts/master_log_table_query.py
+++ Database/Big_Query/Creation_scripts/master_log_table_query.py
@@ -3,105 +3,122 @@ import datetime as DT
 import pandas as pd
 
 def log_table():
+	##################################Set Date and take previous day for data agregation##############
 	today = DT.date.today()
 	day_before = today - DT.timedelta(days=1)
-	client = bigquery.Client()
-	########################################Twitter logs Query################################
+	client = bigquery.Client()  
+	########################################Twitter logs Query#############################################
 	query_twitter = client.query("""SELECT constituent_name, sum(downloaded_tweets) as tweets
 	FROM `igenie-project.pecten_dataset_test.tweet_logs`
 	where date = timestamp('{}')
 	GROUP BY constituent_name""".format(day_before))
 	tweet_results = query_twitter.result()
-	constituent_name = []
+	######Initialise empty list and append results from BQ######
+	constituent_name_twitter = [] 
 	tweets = []
 	for row in tweet_results:
-		constituent_name.append(row.constituent_name)
+		constituent_name_twitter.append(row.constituent_name)
 		tweets.append(row.tweets)
 	date = []
-	for i in constituent_name:
+	for i in constituent_name_twitter:
 		date.append(day_before)	
-	tweets_df = pd.DataFrame({'Date':date,'Constituent_name':constituent_name,
+	##########Merge the list into pandas dataframe######
+	tweets_df = pd.DataFrame({'Date':date,'Constituent_name':constituent_name_twitter,
 							'tweets':tweets})
 	######################################Bloomberg log query################################
 	query_bloomberg = client.query("""SELECT constituent_name, sum(downloaded_news) as bloomberg FROM `igenie-project.pecten_dataset_test.news_logs` 
 	where date = TIMESTAMP('{}') and source = 'Bloomberg'
 	GROUP BY constituent_name""".format(day_before))
 	bloomberg_results = query_bloomberg.result()
-	constituent_name1 = []
+	######Initialise empty list and append results from BQ####
+	constituent_name_bloomberg = []
 	bloomberg = []
 	for row in bloomberg_results:
-		constituent_name1.append(row.constituent_name)
+		constituent_name_bloomberg.append(row.constituent_name)
 		bloomberg.append(row.bloomberg)
-	bloomberg_df = pd.DataFrame({'Constituent_name':constituent_name1,
+	######Merge the list into pandas dataframe######
+	bloomberg_df = pd.DataFrame({'Constituent_name':constituent_name_bloomberg,
 								'bloomberg':bloomberg})
-	collect = pd.merge(tweets_df,bloomberg_df,on ='Constituent_name',how='left')
-	#print(collect2)
+	######Merge the two dataframes based on key value Constituent_name#######							
+	bloomberg_merge = pd.merge(tweets_df,bloomberg_df,on ='Constituent_name',how='left')
 	##########################################Orbis Log Query################################
 	query_orbis = client.query("""SELECT constituent_name, sum(downloaded_news) as orbis FROM `igenie-project.pecten_dataset_test.news_logs` 
 	where date = TIMESTAMP('{}') and source = 'Orbis'
 	GROUP BY constituent_name""".format(day_before))
 	orbis_results = query_orbis.result()
-	constituent_name2 = []
+	constituent_name_orbis = []
 	orbis = []
 	for row in orbis_results:
-		constituent_name2.append(row.constituent_name)
+		constituent_name_orbis.append(row.constituent_name)
 		orbis.append(row.orbis)
-	orbis_df = pd.DataFrame({'Constituent_name':constituent_name2,
+	####Merge the list into pandas dataframe######
+	orbis_df = pd.DataFrame({'Constituent_name':constituent_name_orbis,
 								'orbis':orbis})
-	collect1 = pd.merge(collect,orbis_df,on='Constituent_name',how='left')
-	########################################rss_feeds######################
+	####Merge the previous dataframe with orbis_df based on Constituent_name#########
+	orbis_merge = pd.merge(bloomberg_merge,orbis_df,on='Constituent_name',how='left')
+	########################################rss_feeds###########################################
 	query_rss = client.query("""SELECT constituent_name, sum(downloaded_news) as rss FROM `igenie-project.pecten_dataset_test.news_logs` 
 	where date = TIMESTAMP('{}') and source = 'Yahoo Finance RSS'
 	GROUP BY constituent_name""".format(day_before))
 	rss_results = query_rss.result()
-	constituent_name3 = []
+	#####Initialise empty list and append result from BQ####
+	constituent_name_rss = []
 	rss_feeds = []
 	for row in rss_results:
-		constituent_name3.append(row.constituent_name)
+		constituent_name_rss.append(row.constituent_name)
 		rss_feeds.append(row.rss)
-	rss_df = pd.DataFrame({'Constituent_name':constituent_name3,
+	######Merge list into pandas dataframe####
+	rss_df = pd.DataFrame({'Constituent_name':constituent_name_rss,
 								'rss_feeds':rss_feeds})
-	collect2 = pd.merge(collect1,rss_df,on='Constituent_name',how='left')
-	#print(collect2)
+	######Merge rss dataframe with the previous dataframe based on constituent_name#######
+	rss_merge = pd.merge(orbis_merge,rss_df,on='Constituent_name',how='left')
 	#############################################stocktwits######################
 	query_stocktwits = client.query("""SELECT constituent_name, count(*) as stocktwits FROM `igenie-project.pecten_dataset_test.tweets` 
 	where source = 'StockTwits' and date = TIMESTAMP('{}')
 	group by constituent_name""".format(day_before))
 	stocktwits_results = query_stocktwits.result()
+	######Initialise list with a dummy value because of the possibility of query result being nil for stocktwits######################
 	stocktwits = [0]
-	constituent_name4 = ['E.ON SE']
+	constituent_name_stocktwits = ['E.ON SE']
 	for row in stocktwits_results:
-		constituent_name4.append(row.constituent_name)
+		constituent_name_stocktwits.append(row.constituent_name)
 		stocktwits.append(row.stocktwits)
+	##########Check if any additional results are appended. Yes: Retain the dummy value which is zero. No: Remove dummy value#####
 	if(len(stocktwits)==1):
 		pass
 	else:
 		del stocktwits[0]
-		del constituent_name4[0]
-	stocktwits_df = pd.DataFrame({'Constituent_name':constituent_name4,
+		del constituent_name_stocktwits[0]
+	########Merge the list into pandas data frame########
+	stocktwits_df = pd.DataFrame({'Constituent_name':constituent_name_stocktwits,
 									'stocktwits':stocktwits})
-	collect3 = pd.merge(collect2, stocktwits_df,on='Constituent_name',how='left')
-	#print(collect3)
-	##########################################ticker##############################
+	######Merge the two dataframes
+	stocktwits_merge = pd.merge(rss_merge, stocktwits_df,on='Constituent_name',how='left')
+	##########################################ticker log query##############################
 	query_ticker = client.query("""SELECT constituent_name, sum(downloaded_ticks) as ticker FROM `igenie-project.pecten_dataset_test.ticker_logs` 
 	where date = TIMESTAMP('{}')
 	group by constituent_name""".format(day_before))
 	ticker_results = query_ticker.result()
+	##############Initialise list with a dummy value because of the possibility of query result being nil for ticker#####
 	ticker = [0]
-	constituent_name5 = ['E.ON SE']
+	constituent_name_ticker = ['E.ON SE']
 	for row in ticker_results:
-		constituent_name5.append(row.constituent_name)
+		constituent_name_ticker.append(row.constituent_name)
 		ticker.append(row.ticker)
+	##########Check if any additional results are appended. Yes: Retain the dummy value which is zero. No: Remove dummy value#####
 	if(len(ticker)==1):
 		pass
 	else:
 		del ticker[0]
-		del constituent_name5[0]
-	ticker_df = pd.DataFrame({'Constituent_name':constituent_name5,
+		del constituent_name_ticker[0]
+	#####Merge the lists into pandas dataframe###############
+	ticker_df = pd.DataFrame({'Constituent_name':constituent_name_ticker,
 								'ticker':ticker})
-	collect4 = pd.merge(collect3, ticker_df,on='Constituent_name', how = 'left')
-	collect5 = collect4.fillna(0)
-	print(collect5)
+	#####Merge ticker dataframe with previous dataframes#########
+	ticker_merge = pd.merge(stocktwits_merge, ticker_df,on='Constituent_name', how = 'left')
+	#############Replace Nan in dataframes with zero#######
+	df = ticker_merge.fillna(0)
+	print(df)
 	#print(collect4.iloc[[0],[3]])
 	constituent = collect5.iloc[:,0]
 	Date = collect5.iloc[:,1]
