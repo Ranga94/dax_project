1debc88882dc626d1ea32493bd22dc6f3ab7eaf8
Ranga <rangavittalprasad@gmail.com>
2018-02-02 16:02:57 +0530

    code changes

--- Database/Big_Query/BQ_mode_changes_python_script/data_collection_copy.py
+++ Database/Big_Query/BQ_mode_changes_python_script/data_collection_copy.py
@@ -27,11 +27,11 @@ if __name__ == '__main__':
 	#data_collection_tables = ["all_news","analyst_opinions","business_ratio","dividend","frankfurt_trading_parameters","historical",
 	#							"historical_key_data","instrument_information","liquidity","ma_deals","master_data","news_logs","recent_report",
 	#							"technical_figures","ticker_data","ticker_logs","trading_parameters","tweet_logs","tweets"]
-	data_collection_tables = ["all_news","analyst_opinions"]
+	data_collection_tables = ["dividend","analyst_opinions"]
 	for table in data_collection_tables:
-		copy_table("pecten_dataset_test","pecten_dataset_new",table,table+"_94","igenie-project")
-		drop_table("pecten_dataset_new",table)
-		copy_table("pecten_dataset_new","pecten_dataset_new",table+"_94",table,"igenie-project")
-		drop_table("pecten_dataset_new",table+"_94")
+		copy_table("pecten_dataset_test",sys.argv[1],table,table+"_94","igenie-project")
+		drop_table(sys.argv[1],table)
+		copy_table(sys.argv[1],sys.argv[1],table+"_94",table,"igenie-project")
+		drop_table(sys.argv[1],table+"_94")
 		print("--------------------------------------")
 	
