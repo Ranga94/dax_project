85be3efc3dfb751a2167e4c6cab18ce0825dcaac
Ranga <rangavittalprasad@gmail.com>
2018-02-02 16:08:08 +0530

    code changes

--- Database/Big_Query/BQ_mode_changes_python_script/data_collection_copy.py
+++ Database/Big_Query/BQ_mode_changes_python_script/data_collection_copy.py
@@ -29,9 +29,7 @@ if __name__ == '__main__':
 	#							"technical_figures","ticker_data","ticker_logs","trading_parameters","tweet_logs","tweets"]
 	data_collection_tables = ["dividend","analyst_opinions"]
 	for table in data_collection_tables:
-		copy_table("pecten_dataset_test",sys.argv[1],table,table+"_94","igenie-project")
 		drop_table(sys.argv[1],table)
-		copy_table(sys.argv[1],sys.argv[1],table+"_94",table,"igenie-project")
-		drop_table(sys.argv[1],table+"_94")
+		copy_table("pecten_dataset_test",sys.argv[1],table,table,"igenie-project")
 		print("--------------------------------------")
 	
