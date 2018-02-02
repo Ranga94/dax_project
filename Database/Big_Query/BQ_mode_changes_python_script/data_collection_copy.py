b6925c70e4bae5fa2da0384928b17d04c8792044
Ranga <rangavittalprasad@gmail.com>
2018-02-02 14:48:39 +0530

    code changes

--- Database/Big_Query/BQ_mode_changes_python_script/data_collection_copy.py
+++ Database/Big_Query/BQ_mode_changes_python_script/data_collection_copy.py
@@ -13,5 +13,16 @@ def copy_table(from_dataset_id, to_dataset_id, table_id, new_table_id, project):
 	copy_job.result()
 	print('Table {} copied to {}.'.format(table_id, new_table_id))
 	
+def drop_table(dataset_name,table_name):
+	print("Dropping table {}".format(table_name))
+	client = bigquery.Client()
+	dataset_ref = client.dataset(dataset_name)
+	dataset = client.get_dataset(dataset_ref)
+	table_ref = dataset.table(table_name)
+	table = client.get_table(table_ref)
+	client.delete_table(table)
+	
 if __name__ == '__main__':
-	copy_table("pecten_dataset_test","pecten_dataset_new","dividend","divedend_94","igenie-project")
+	#copy_table("pecten_dataset_test","pecten_dataset_new","dividend","divedend_94","igenie-project")
+	drop_table("pecten_dataset_new","dividend")
+	
