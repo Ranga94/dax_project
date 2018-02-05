57d09245cada127533d224ce93b513c50943d38d
Ranga <rangavittalprasad@gmail.com>
2018-02-05 14:40:32 +0530

    code changes

--- Database/Big_Query/Creation_scripts/back_up.py
+++ Database/Big_Query/Creation_scripts/back_up.py
@@ -1,5 +1,7 @@
 import requests as req
 
+bigquery_client = bigquery.Client()
+
 resp = req.get("https://www.googleapis.com/bigquery/v2/projects/igenie-project/datasets/pecten_dataset_dev/tables")
 
 print(resp.text)
