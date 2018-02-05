2c604afb814eb1994e8493502ca39241578a9141
Ranga <rangavittalprasad@gmail.com>
2018-02-05 14:59:14 +0530

    code changes

--- Database/Big_Query/Creation_scripts/back_up.py
+++ Database/Big_Query/Creation_scripts/back_up.py
@@ -3,6 +3,6 @@ from google.cloud import bigquery
 
 client = bigquery.Client(project='igenie-project')
 dataset_ref = client.dataset("pecten_dataset_dev")
-dataset = bigquery.Dataset(dataset_ref)
 
-tables = list(client.list_tables(dataset))
+
+tables = list(client.list_tables(dataset_ref))
