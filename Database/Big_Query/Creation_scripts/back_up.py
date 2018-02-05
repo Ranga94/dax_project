2b62c46e25d27ba6fbccfe00c472d34906f59f13
Ranga <rangavittalprasad@gmail.com>
2018-02-05 14:48:54 +0530

    code changes

--- Database/Big_Query/Creation_scripts/back_up.py
+++ Database/Big_Query/Creation_scripts/back_up.py
@@ -1,9 +1,5 @@
 import requests as req
 from google.cloud import bigquery
 
-
-bigquery_client = bigquery.Client()
-
-resp = req.get("https://www.googleapis.com/bigquery/v2/projects/igenie-project/datasets/pecten_dataset_dev/tables")
-
-print(resp.text)
+client = bigquery.Client(project='igenie-project')
+tables = list(client.list_tables('pecten_dataset_dev'))
