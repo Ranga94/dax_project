ad513a880f3246131a722f0a7f38811a8a4a8778
Ranga <rangavittalprasad@gmail.com>
2018-02-06 12:14:49 +0530

    code changes

---
+++ Database/Big_Query/Creation_scripts/twitter_credentials.py
@@ -0,0 +1,5 @@
+#Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "803580271115255808-FCuEYBBkihkPMwu3h495AdLXm8bYeiG"
+ACCESS_TOKEN_SECRET = "aM60itiTpIa567XYITmzOXuawEw6TKnf78RnRUDjLPahA"
+CONSUMER_KEY = "3xWdzB0vRQYNg6a8GfCrLMpz5"
+CONSUMER_SECRET = "PbX4puZiYzLtM32h8IUoQ2JnC6gpeNqd1x8YmlrTZdTCNv3TDr"
