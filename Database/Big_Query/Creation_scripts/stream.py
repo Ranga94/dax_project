798a5431ff8c96b4856e2fb27e4dca24f19d95e9
Ranga <rangavittalprasad@gmail.com>
2018-02-06 16:12:49 +0530

    code changes

--- Database/Big_Query/Creation_scripts/stream.py
+++ Database/Big_Query/Creation_scripts/stream.py
@@ -21,10 +21,14 @@ class StdOutListener(StreamListener):
 		#favourites_count = python_obj["favourites_count"]
 		#followers_count = python_obj["followers_count"]
 		client = bigquery.Client()
-		query_insert =  client.query("""INSERT INTO `igenie-project.pecten_dataset_dev.stream_twitter`
-		(created_at,text) 
-		VALUES ('{}','{}')""".format(created_at,text))
-		insert_result = query_insert.result()
+		try: 
+			query_insert =  client.query("""INSERT INTO `igenie-project.pecten_dataset_dev.stream_twitter`
+			(created_at,text) 
+			VALUES ('{}','{}')""".format(created_at,text))
+			insert_result = query_insert.result()
+		except Exception as e:
+			print(e)
+			
 		#print(screen_name)
 		#print(followers_count)
 		#print(language)
