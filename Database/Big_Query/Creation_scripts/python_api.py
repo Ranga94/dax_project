44eb84a7cd83d187ff202b425fb5a21197982e46
Ranga <rangavittalprasad@gmail.com>
2018-02-12 14:51:07 +0530

    code changes

--- Database/Big_Query/Creation_scripts/python_api.py
+++ Database/Big_Query/Creation_scripts/python_api.py
@@ -7,7 +7,7 @@ for dataset_id in DATASET_ID:
 	dataset = bigquery.Dataset(client.dataset(dataset_id))
 	tables = list(client.list_dataset_tables(dataset))
 	#print(tables)
-
+"""
 	for table in tables:
 		table_ref = dataset.table(table.table_id)
 		#print(table.table_id)
@@ -18,5 +18,12 @@ for dataset_id in DATASET_ID:
 			job = client.extract_table(table_ref, destination,job_config= job_config)
 			job.result()
 		except Exception as e:
-			print(e)
-		#print('Exported {} to {}'.format(table.table_id, destination)
+			print(e)"""
+	#print('Exported {} to {}'.format(table.table_id, destination)
+	##For tables containing data of more than 1 GB
+	big_table = ["all_news","tweets","tweets_unmodified"]
+	for table in big_table:
+		table_ref = dataset.table(table)
+		destination = "gs://pecten_dataset_t/"+table.table_id+"-*.json"
+		job_config.destination_format = 'NEWLINE_DELIMITED_JSON'
+		job = client.extract_table(table_ref, destination,job_config= job_config)
