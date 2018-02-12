215ecbeb0a525cdec44b96d09dec3c6f65560c88
Ranga <rangavittalprasad@gmail.com>
2018-02-12 12:31:56 +0530

    code changes

--- Database/Big_Query/Creation_scripts/python_api.py
+++ Database/Big_Query/Creation_scripts/python_api.py
@@ -4,5 +4,9 @@ client = bigquery.Client()
 DATASET_ID = "pecten_dataset_test"
 dataset = bigquery.Dataset(client.dataset(DATASET_ID))
 tables = list(client.list_dataset_tables(dataset))
-print(tables.table_id)
+print(tables)
+
+for table in tables:
+	source_table_ref = source_dataset.table(table.table_id)
+	print(source_table_ref)
 
