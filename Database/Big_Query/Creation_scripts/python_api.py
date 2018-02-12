f58daa74cb8682fdfcd236aa6ed2fd649d6a697e
Ranga <rangavittalprasad@gmail.com>
2018-02-12 15:22:26 +0530

    code changes

--- Database/Big_Query/Creation_scripts/python_api.py
+++ Database/Big_Query/Creation_scripts/python_api.py
@@ -3,24 +3,24 @@ from google.cloud import bigquery
 client = bigquery.Client()
 job_config = bigquery.ExtractJobConfig()
 def export_data():
-	DATASET_ID = ["pecten_dataset_test"]
+	DATASET_ID = ["pecten_dataset_test","pecten_dataset_dev","pecten_dataset"]
 
 	for dataset_id in DATASET_ID:
 		dataset = bigquery.Dataset(client.dataset(dataset_id))
 		tables = list(client.list_dataset_tables(dataset))
 		#print(tables)
-"""
+
 		for table in tables:
 			table_ref = dataset.table(table.table_id)
 			#print(table.table_id)
 			try:
-				destination = "gs://pecten_dataset_t/"+table.table_id+".json"
+				destination = "gs://"+dataset_id+"/"+table.table_id+".json"
 				
 				job_config.destination_format = 'NEWLINE_DELIMITED_JSON'
 				job = client.extract_table(table_ref, destination,job_config= job_config)
 				job.result()
 			except Exception as e:
-				print(e)"""
+				print(e)
 			#print('Exported {} to {}'.format(table.table_id, destination)
 			
 ##For tables containing data of more than 1 GB			
@@ -31,11 +31,11 @@ def export_big_table():
 		big_table = ["all_news","tweets","tweets_unmodified"]
 		for table in big_table:
 			table_ref = dataset.table(table)
-			destination = "gs://pecten_dataset_t/"+table+"/"+table+"-*.json"
+			destination = "gs://"+DATASET_ID+"/"+table+"/"+table+"-*.json"
 			job_config.destination_format = 'NEWLINE_DELIMITED_JSON'
 			job = client.extract_table(table_ref, destination,job_config= job_config)
 			job.result()
 			
 if __name__ == '__main__':
-	#export_data()
+	export_data()
 	export_big_table()
