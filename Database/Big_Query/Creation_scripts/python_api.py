44ddc7d7ca14665027a4f5ff73f35e80b095b166
Ranga <rangavittalprasad@gmail.com>
2018-02-12 12:46:20 +0530

    code changes

--- Database/Big_Query/Creation_scripts/python_api.py
+++ Database/Big_Query/Creation_scripts/python_api.py
@@ -4,12 +4,12 @@ client = bigquery.Client()
 DATASET_ID = "pecten_dataset_test"
 dataset = bigquery.Dataset(client.dataset(DATASET_ID))
 tables = list(client.list_dataset_tables(dataset))
-print(tables)
+#print(tables)
 
 for table in tables:
 	table_ref = dataset.table(table.table_id)
 	#print(table.table_id)
 	destination = "gs://pecten_dataset_t/"+table.table_id
-	job = bigquery_client.extract_table(table_ref, destination)
-	job.result()
-	print('Exported {} to {}'.format(table.table_id, destination)
+	#job = bigquery_client.extract_table(table_ref, destination)
+	#job.result()
+	#print('Exported {} to {}'.format(table.table_id, destination)
