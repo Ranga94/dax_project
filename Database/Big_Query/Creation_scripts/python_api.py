cb1f538be8f60702f25f6d12f92237961393d0d3
Ranga <rangavittalprasad@gmail.com>
2018-02-12 12:47:21 +0530

    code changes

--- Database/Big_Query/Creation_scripts/python_api.py
+++ Database/Big_Query/Creation_scripts/python_api.py
@@ -10,6 +10,7 @@ for table in tables:
 	table_ref = dataset.table(table.table_id)
 	#print(table.table_id)
 	destination = "gs://pecten_dataset_t/"+table.table_id
+	print(destination)
 	#job = bigquery_client.extract_table(table_ref, destination)
 	#job.result()
 	#print('Exported {} to {}'.format(table.table_id, destination)
