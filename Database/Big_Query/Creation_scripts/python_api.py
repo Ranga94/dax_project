504432d6114006b8cd2363990391b9c9eeffdc9c
Ranga <rangavittalprasad@gmail.com>
2018-02-12 15:53:16 +0530

    code changes

--- Database/Big_Query/Creation_scripts/python_api.py
+++ Database/Big_Query/Creation_scripts/python_api.py
@@ -33,7 +33,7 @@ def export_big_table():
 		for table_id in big_table:
 			table_ref = dataset_ref.table(table_id)
 			print(table_ref)
-			destination = "gs://"+str(dataset)+"/"+table+"/"+table+"-*.json"
+			destination = "gs://"+str(dataset)+"/"+table_id+"/"+table_id+"-*.json"
 			job_config.destination_format = 'NEWLINE_DELIMITED_JSON'
 			job = client.extract_table(table_ref, destination,job_config= job_config)
 			job.result()
