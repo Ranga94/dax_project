a4576706b41219d0bce5dae16a2d065f3523a4c8
Ranga <rangavittalprasad@gmail.com>
2018-02-12 14:20:53 +0530

    code changes

--- Database/Big_Query/Creation_scripts/python_api.py
+++ Database/Big_Query/Creation_scripts/python_api.py
@@ -11,7 +11,7 @@ for dataset_id in DATASET_ID:
 	for table in tables:
 		table_ref = dataset.table(table.table_id)
 		#print(table.table_id)
-		destination = "gs://pecten_dataset_t"+table.table_id+"-*.json"
+		destination = "gs://pecten_dataset_t/"+table.table_id+"-*.json"
 		job_config = bigquery.ExtractJobConfig()
 		job_config.destination_format = 'NEWLINE_DELIMITED_JSON'
 		job = client.extract_table(table_ref, destination,job_config= job_config)
