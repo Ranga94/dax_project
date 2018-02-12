40fb55e0b19a9839b3c640b52a0036ce21636eef
Ranga <rangavittalprasad@gmail.com>
2018-02-12 16:10:32 +0530

    code changes

--- Database/Big_Query/Creation_scripts/python_api.py
+++ Database/Big_Query/Creation_scripts/python_api.py
@@ -6,23 +6,23 @@ def export_data():
 	DATASET_ID = ["pecten_dataset_test","pecten_dataset_dev","pecten_dataset"]
 
 	for dataset_id in DATASET_ID:
-		dataset = bigquery.Dataset(client.dataset(dataset_id))
+		dataset = client.dataset(dataset_id))
 		tables = list(client.list_dataset_tables(dataset))
-		#print(tables)
+		print(tables)
 
 		for table in tables:
 			table_ref = dataset.table(table.table_id)
 			print(table_ref)
 			print(table.table_id)
+			destination = "gs://"+dataset_id+"/"+table.table_id+".json"
+			job_config.destination_format = 'NEWLINE_DELIMITED_JSON'
+			print(destination)
 			try:
-				destination = "gs://"+dataset_id+"/"+table.table_id+".json"
-				print(destination)
-				job_config.destination_format = 'NEWLINE_DELIMITED_JSON'
 				job = client.extract_table(table_ref, destination,job_config= job_config)
 				job.result()
 			except Exception as e:
 				print(e)
-			#print('Exported {} to {}'.format(table.table_id, destination)
+			print('Exported {} to {}'.format(table.table_id, destination)
 			
 ##For tables containing data of more than 1 GB			
 def export_big_table():
@@ -39,5 +39,5 @@ def export_big_table():
 			job.result()
 			
 if __name__ == '__main__':
-	#export_data()
+	export_data()
 	export_big_table()
