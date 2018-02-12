706330aef71dbb3dd0ca341eafa6fff870a9cb7f
Ranga <rangavittalprasad@gmail.com>
2018-02-12 12:20:20 +0530

    code changes

--- Database/Big_Query/Creation_scripts/python_api.py
+++ Database/Big_Query/Creation_scripts/python_api.py
@@ -1,6 +1,6 @@
 from google.cloud import bigquery
 
-bigquery_client = bigquery.Client()
+client = bigquery.Client()
 DATASET_ID = "pecten_dataset_test"
 dataset = bigquery.Dataset(client.dataset(DATASET_ID))
 tables = list(client.list_dataset_tables(dataset))
