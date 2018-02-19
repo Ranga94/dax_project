5d3b2ea27536bcd75f6c8cd33deaea9d2864d312
Ranga <rangavittalprasad@gmail.com>
2018-02-19 15:07:37 +0530

    code changes

--- Database/Big_Query/Creation_scripts/reuters_analyst_rating.py
+++ Database/Big_Query/Creation_scripts/reuters_analyst_rating.py
@@ -8,13 +8,73 @@ client = bigquery.Client()
 headers = ['1-5 Linear Scale','(1) BUY','(2) OUTPERFORM','(3) HOLD','(4) UNDERPERFORM','(5) SELL','No Opinion','NaN','Mean Rating','constituent']
 df_res = pd.DataFrame(columns = headers)
 #print(df_res)
-constituent_name = ['adidas','allianz',]
+constituent_name = ['Adidas' ,'Allianz','BASF','Bayer','BMW','Beiersdorf AG','Commerzbank','Continental','Daimler','Deutsche Bank','Deutsche Boerse',
+'Deutsche Lufthansa','Deutsche Post','Deutsche Telekom','E.ON','Fresenius Medical','Fresenius SE','HeidelbergCement','Henkel','Infineon','Linde','Merck','Munich Re Group',
+'Prosiebensat1','RWE','SAP','Siemens','ThyssenKrupp','Volkswagen','Vonovia']
 for constituent in constituent_name:
-    if constituent == 'allianz':
+    if constituent == 'Allianz':
         name = 'ALVG.DE'
-    elif constituent == 'adidas':
+    elif constituent == 'Adidas':
         name = 'ADSGn.DE'
-    dfs = pd.read_html('https://www.reuters.com/finance/stocks/analyst/{}'.format(name))
+	elif constituent == 'BASF':
+		name = 'BASFn.DE'
+	elif constituent = 'Bayer'
+		name = 'BAYGn.DE'
+	elif constituent = 'BMW'
+		 name = 'BMWG.DE'
+	elif constituent = 'Beiersdorf AG'
+		name = 'BEIG.DE'
+	elif constituent = 'Commerzbank'
+		name = 'CBKG.DE'
+	elif constituent = 'Continental'
+		name = 'CONG.DE'
+	elif constituent = 'Daimler'
+		name = 'DAIGn.DE'
+	elif constituent = 'Deutsche Bank'
+		name = 'DBKGn.DE'
+	elif constituent = 'Deutsche Boerse'
+		name = 'DB1Gn.DE'
+	elif constituent = 'Deutsche Lufthansa'
+		name = 'LHAG.DE'
+	elif constituent = 'Deutsche Post'
+		name = 'DPWGn.DE'
+	elif constituent = 'Deutsche Telekom'
+		name = 'DTEGn.DE'
+	elif constituent = 'E.ON'
+		name = 'EONGn.DE'
+	elif constituent = 'Fresenius Medical'
+		name = 'FMEG.DE'
+	elif constituent = 'Fresenius SE'
+		name = 'FREG.DE'
+	elif constituent = 'HeidelbergCement'
+		name = 'HEIG.DE'
+	elif constituent = 'Henkel'
+		name = 'HNKG_p.DE'
+	elif constituent = 'Infineon'
+		name = 'IFXGn.DE'
+	elif constituent = 'Linde'
+		name = 'LIN1.DE'
+	elif constituent = 'Merck'
+		name = 'MRCG.DE'
+	elif constituent = 'Munich Re Group'
+		name = 'MUVGn.DE'
+	elif constituent = 'Prosiebensat1'
+		name = 'PSMGn.DE'
+	elif constituent = 'RWE'
+		name = 'RWEG.DE'
+	elif constituent = 'SAP'
+		name = 'SAPG.DE'
+	elif constituent = 'Siemens'
+		name = 'SIEGn.DE'
+	elif constituent = 'ThyssenKrupp'
+		name = 'TKAG.DE'
+	elif constituent = 'Volkswagen'
+		name = 'VOWG_p.DE'
+	elif constituent = 'Vonovia'
+		name = 'VNAn.DE'		
+	
+		
+	dfs = pd.read_html('https://www.reuters.com/finance/stocks/analyst/{}'.format(name))
     table = dfs[1]
     
     df1 = table[[0,1]]
