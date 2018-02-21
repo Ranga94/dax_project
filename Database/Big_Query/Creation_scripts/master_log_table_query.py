c31565e23b89f8bf56a1b61f99811b501c768099
Ranga <rangavittalprasad@gmail.com>
2018-02-21 23:54:51 +0530

    code changes

--- Database/Big_Query/Creation_scripts/master_log_table_query.py
+++ Database/Big_Query/Creation_scripts/master_log_table_query.py
@@ -32,7 +32,8 @@ def log_table(dataset_id):
 	##########Merge the list into pandas dataframe######
 	tweets_df = pd.DataFrame({'Date':date,'Constituent_name':constituent_name_twitter,
 							'tweets':tweets})
-	tweets_df = pd.merge(tweets_df,constituent_name_pd, on = 'Constituent_name',how='left')						
+	tweets_df = pd.merge(tweets_df,constituent_name_pd, on = 'Constituent_name',how='left')
+	print(tweets_df)
 	######################################Bloomberg log query################################
 	query_bloomberg = client.query("""SELECT constituent_name, sum(downloaded_news) as bloomberg FROM `igenie-project.{}.news_logs` 
 	where date = TIMESTAMP('{}') and source = 'Bloomberg'
@@ -126,7 +127,7 @@ def log_table(dataset_id):
 	ticker_merge = pd.merge(stocktwits_merge, ticker_df,on='Constituent_name', how = 'left')
 	#############Replace Nan in dataframes with zero#######
 	df = ticker_merge.fillna(0)
-	print(df)
+	#print(df)
 	##########################Split dataframes for data insertion##########
 	constituent = df.iloc[:,0]
 	Date = df.iloc[:,1]
@@ -137,11 +138,11 @@ def log_table(dataset_id):
 	StockTwits = df.iloc[:,6]
 	Ticker = df.iloc[:,7]
 	
-	for i in range(0,len(constituent)):
+	"""for i in range(0,len(constituent)):
 		if (Tweets[i] == 0):
 			print("No tweets recieved for {}".format(constituent[i]))
 		elif (Ticker[i] == 0):
-			print("No ticker data collected for {}".format(constituent[i]))
+			print("No ticker data collected for {}".format(constituent[i]))"""
 	######################################Insert into table###########################
 	#for i in range(0,len(constituent)):
 	
