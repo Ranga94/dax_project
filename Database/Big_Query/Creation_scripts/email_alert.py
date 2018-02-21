2bf6d0940b28c6cfd96819707e34223891d689fb
Ranga <rangavittalprasad@gmail.com>
2018-02-21 22:27:39 +0530

    code changes

--- Database/Big_Query/Creation_scripts/email_alert.py
+++ Database/Big_Query/Creation_scripts/email_alert.py
@@ -31,5 +31,7 @@ for row in tweet_results:
 
 twitter_df =pd.DataFrame({'constituent_name':constituent_name_twitter,'tweets':tweets})
 twitter_merge = pd.merge(constituent_name_pd, twitter_df, on = 'constituent_name', how = 'left')
-print(twitter_merge)
+#print(twitter_merge)
+for row_index, row in twitter_merge.iterrows():
+	print row_index, row
 
